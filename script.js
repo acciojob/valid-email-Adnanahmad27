e6fe33ebe0574f24b4f187ac94e436c8ca33334a
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-13 06:16:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function validEmail(str) {
   //your JS code here.
-}
+	// var regex = /^[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,3}$/;
+    // Test the email address against the regular expression pattern
 
+	let regex = /^[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*\.[a-zA-z]{2,3}$/;
+    return regex.test(str); 
+} 
+ 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
